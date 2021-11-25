c705b7c3cb25661f0c93c61036f0d9f88f044b8d
nao-ri <whiteblue0325@gmail.com>
2021-11-25 15:03:54 +0900

    GRAYスケール化

--- buildapp/app/dist/build/CMakeFiles/cppmain.dir/DependInfo.cmake
+++ buildapp/app/dist/build/CMakeFiles/cppmain.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/app/src/main.cpp" "/app/dist/build/CMakeFiles/cppmain.dir/src/main.cpp.o"
+  "/buildapp/app/src/main.cpp" "/buildapp/app/dist/build/CMakeFiles/cppmain.dir/src/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "Clang")
 
--- buildapp/app/dist/build/cmake_install.cmake
+++ buildapp/app/dist/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /app
+# Install script for directory: /buildapp/app
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/app/dist/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/buildapp/app/dist/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
