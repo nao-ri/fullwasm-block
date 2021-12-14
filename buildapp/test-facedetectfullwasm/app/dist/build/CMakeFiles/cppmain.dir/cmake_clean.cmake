b74c2e7b07b5ba2c865f162d4a4ec29ed5f767d2
nao-ri <whiteblue0325@gmail.com>
2021-12-14 22:53:19 +0900

    facedetecltsample

---
+++ buildapp/test-facedetectfullwasm/app/dist/build/CMakeFiles/cppmain.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cppmain.dir/src/main.cpp.o"
+  "cppmain.pdb"
+  "cppmain.js"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cppmain.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
